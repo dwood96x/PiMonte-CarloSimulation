86caae4a802508842b232d609c4659048e42bb0d
David Wood <dwood96x@gmail.com>
2018-09-12 11:02:58 -0400

    Created CalcHypotnuse method

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,16 +10,32 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+
+        }
+        public static double CalcHypot(Coordinates coord)
+        {
+            double hypot = Math.Sqrt(Math.Pow(coord.x, 2) + Math.Pow(coord.y, 2));
+            return hypot;
         }
     }
     struct Coordinates
     {
-        double x;
-        double y;
+        public double x;
+        public double y;
         public Coordinates(double x_, double y_)
         {
             x = x_;
             y = y_;
         }
+        /*
+        public double GetX()
+        {
+            return x;
+        }
+        public double GetY()
+        {
+            return y;
+        }
+        */
     }
 }
