eb98692879ad0734da2928259f6894cc4d0541dc
David Wood <dwood96x@gmail.com>
2018-09-12 10:18:12 -0400

    Created Console app and coordinate struct.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    struct Coordinates
+    {
+        double x;
+        double y;
+        public Coordinates(double x_, double y_)
+        {
+            x = x_;
+            y = y_;
+        }
+    }
+}
